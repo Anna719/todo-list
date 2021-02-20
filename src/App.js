353c0f82962f6186c6225bb1f8dbdfde02d4e1cc
Anna Stelmakh <anna.stelmakh@student.tuke.sk>
2021-02-20 15:47:59 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+
+
+    <div className='todo-app'>
+
+      <TodoList/>
+    </div>
+
+  );
+}
+
+export default App;
